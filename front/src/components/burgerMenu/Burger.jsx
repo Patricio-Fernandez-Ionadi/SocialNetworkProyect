00a6b4a16550b23411826fd35197003e4b626837
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2024-03-13 00:08:34 -0300

    wip actualizacion de store

--- front/src/components/burgerMenu/Burger.jsx
+++ front/src/components/burgerMenu/Burger.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { turnMenu } from '../../store'
 
 export function Burger() {
 	const [openMenu, setOpenMenu] = useState(false)
 
+	const dispatch = useDispatch()
+	const applicationStates = useSelector(({ application }) => application)
+
 	const handleBurgerIcon = () => {
 		setOpenMenu(!openMenu)
+		dispatch(turnMenu(!openMenu))
 	}
+	console.log(applicationStates)
 
 	return (
 		<div className="burger" onClick={handleBurgerIcon}>
