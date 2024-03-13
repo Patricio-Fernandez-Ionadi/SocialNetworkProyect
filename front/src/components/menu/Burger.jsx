3cdfa150f3a997633aaae66af1cf28149e978dd6
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2024-03-13 18:43:54 -0300

    cambio de ruta

--- front/src/components/menu/Burger.jsx
+++ front/src/components/menu/Burger.jsx
@@ -1,24 +1,20 @@
-import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { turnMenu } from '../../store'
 
 export function Burger() {
-	const [openMenu, setOpenMenu] = useState(false)
-
 	const dispatch = useDispatch()
 	const applicationStates = useSelector(({ application }) => application)
+	const { menu_open } = applicationStates
 
 	const handleBurgerIcon = () => {
-		setOpenMenu(!openMenu)
-		dispatch(turnMenu(!openMenu))
+		dispatch(turnMenu(!menu_open))
 	}
-	console.log(applicationStates)
 
 	return (
 		<div className="burger" onClick={handleBurgerIcon}>
 			<div
 				className={
-					openMenu
+					menu_open
 						? 'icon-menu__bar icon-menu__bar--left-arrow'
 						: 'icon-menu__bar'
 				}
